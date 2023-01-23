bc5fbbb1911654970f0d6de1ba41764dba573dd6
marina140291 <marina140291@mail.ru>
2023-01-23 22:55:53 +0300

    задача 41

---
+++ задача/задача 43/задача 41.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+void input_array(int[]array)
+{
+        for (int i = 0; i < array.Length; i++) 
+        {
+        Console.Write($"Введите элемент массива № {i+1}: ");
+          array[i] = (int)Convert.ToDouble(Console.ReadLine());
+        }
+        
+}
+
+int release(int[]array)
+{
+int count=0;
+for (int i =0; i<array.Length; i++)
+     if (array[i] > 0)
+         count ++;
+     return count;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[]array=new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(" ,  ", array)}]");
+Console.WriteLine(release (array));
+
